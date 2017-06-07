53625dc9d27ddfa99b3fefea9906217644c9b811
ManjuYadav15 <manjuvaratharasan97@gmail.com>
2017-06-07 13:34:55 +0530

    Create Maximum Draws.cpp

---
+++ Maximum Draws.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n,i,a;
+    cin>>n;
+    for(i=0;i<n;i++)
+        {
+        cin>>a;
+        cout<<(a+1)<<endl;
+    }
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
