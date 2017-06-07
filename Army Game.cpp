8b5539ceaffda980f445ea1491956442b3b72280
ManjuYadav15 <manjuvaratharasan97@gmail.com>
2017-06-07 13:37:04 +0530

    Create Army Game.cpp

---
+++ Army Game.cpp
@@ -0,0 +1,34 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n,a;
+    int m;
+    cin >> n >> m;
+    a=(n+n%2)*(m+m%2)/4;
+    cout<<a<<endl;
+    return 0;
+}
